c4d5ebaba6005c974accce2ab849ccb45056f908
netronamon <skaiamonge@gmail.com>
2022-09-14 01:54:46 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return o 
+ * prints _putchar
+ */
+int main()
+{
+   putchar('_');
+   putchar('p');
+   putchar('u');putchar('t');putchar('c');putchar('h');
+   putchar('a');
+   putchar('r');
+   putchar('\n');
+return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+project 0 deals with _putchar
