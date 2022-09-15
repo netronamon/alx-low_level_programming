738a1e580b4af561d327c8c0190ead2f4ca1df0c
netronamon <skaiamonge@gmail.com>
2022-09-15 02:40:56 -0700

    some adjustments

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,14 @@
  */
 int main()
 {
-   putchar('_');
-   putchar('p');
-   putchar('u');
-   putchar('t');
-   putchar('c');
-   putchar('h');
-   putchar('a');
-   putchar('r');
-   putchar('\n');
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return 0;
 }
