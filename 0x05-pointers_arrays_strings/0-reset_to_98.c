8651f2ff77dab9c4e6804e7c9c31e11e5a26c130
netronamon <skaiamonge@gmail.com>
2022-09-19 11:59:59 -0700

    my first commmit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+/**
+ * file 0-reset_to_98
+ * author:amon
+ */
+#include "main.h"
+/**
+ * reset_to_98 takes a pointer to int and changes its value
+ *             to 98. 
+ * @n pointer to an int
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+/**
+ * all prototypes of project 0x05
+ * nice and simple
+ */
+void reset_to_98(int *n);
