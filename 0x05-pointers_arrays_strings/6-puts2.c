1c477d64a26d271415cbd4fb7398eccd76805578
netronamon <skaiamonge@gmail.com>
2022-09-20 12:23:35 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,15 +9,14 @@
  * puts2 - Prints one char out of two of a string.
  * @str: The string containing characters.
  */
+
 void puts2(char *str)
 {
-	int index = 0, len = 0;
-
-	while (str[index++])
-		len++;
-
-	for (index = 0; index < len; index += 2)
-		_putchar(str[index]);
-
-	_putchar('\n');
+while (*str != '\0')
+{
+printf("%c",*str);
+printf("\n");
+str++;
 }
+}
+
