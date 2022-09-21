214e0f92bc5863e1005b9d5d35f5f8ff1f383f74
netronamon <skaiamonge@gmail.com>
2022-09-20 22:16:01 -0700

    copy a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -17,13 +17,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index] != '\0')
+	while (src[index])
 	{
 		dest[index] = src[index];
 		index++;
 	}
-	index++;
-	dest[index] = src[index];
 
 	return (dest);
 }
