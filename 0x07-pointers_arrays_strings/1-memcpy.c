50165a4dff2568388fb1a04fcd2a8e6b7e3e931e
netronamon <skaiamonge@gmail.com>
2022-09-27 08:04:40 -0700

    memcopy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
 /*
  * File: 1-memcpy.c
- * Auth: Brennan D Baraban
+ * Author:amon
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _memcpy - Copies @n bytes from the memory area pointed
