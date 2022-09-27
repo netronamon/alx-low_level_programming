0c6154551ec70331ec8f5cb4360f151f9d92e79f
netronamon <skaiamonge@gmail.com>
2022-09-27 12:06:35 -0700

    copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -20,7 +20,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
            char *destination = dest;
         char *source = src;
 
-	for (index = 0; index < n; index++)
+	for (index = 0;index < n;index++)
 		destination[index] = source[index];
 
 	return (dest);
