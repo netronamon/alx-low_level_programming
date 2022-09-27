4d11bd32021717d5d4980fa6f4d53f381c4dca00
netronamon <skaiamonge@gmail.com>
2022-09-27 07:49:00 -0700

    my first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,7 +17,7 @@
 void *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *memory = s, value = b;
+        char *memory = s, value = b;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
