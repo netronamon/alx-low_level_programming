0238c0d94f9948b6ac5e89511f5c293b78ef3df0
netronamon <skaiamonge@gmail.com>
2022-10-24 02:24:28 -0700

    using file macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+/* 
+ * file:2-main.c
+ * author:amon
+ * Desc: program that prints the name of the file it is compiled from
+ *
+ */
+int main( void )
+{
+	printf(" %s \n ", __FILE__);
+printf(" \n ");
+return(0);
+}	
