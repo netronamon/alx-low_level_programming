2db2e8c0834426803f6538485476134d545168aa
netronamon <skaiamonge@gmail.com>
2022-10-24 01:48:09 -0700

    third commit

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
 /* 
- * author:amon
- * file:0-object_like_macro.h
- * descriptioin:macros
+ * author:amon file:0-object_like_macro.h
+ * Desc: header file that defines the SIZE and expands to 1024
+ *
  */
 #define SIZE 1024
