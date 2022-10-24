ad4f8f4f78459ec7276b16a13854f78fc4d3808a
netronamon <skaiamonge@gmail.com>
2022-10-24 02:37:33 -0700

    main fixed ;

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,7 +5,13 @@
  * Desc: program that prints the name of the file it is being compiled from
  *
  */
-int main (void )
+
+/**
+ * main - Prints the name of the file of the program
+ *  was compiled from, followed by a new line.
+ * Return: Always 0.
+ */
+int main (void)
 {
 	printf("%s\n", __FILE__);
 	return(0);
